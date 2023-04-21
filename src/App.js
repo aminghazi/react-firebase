2570fe9a0e5cc591adb2c805368093241bac0c51
Amin Ghazi <aminghazi@outlook.com>
2023-04-21 22:31:36 +0330

    register form

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import './input.css'
+import Form from './components/form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Firebase
-        </p>
-        <h1 class="text-3xl font-bold underline">
-          Hello world!
-        </h1>
-        <form className='bg-slate-200'>
-          <input />
-        </form>
-      </header>
+      <Form />
     </div>
   );
 }
